caade20f0ad3230514028853e2700da17e02b1d0
Johannes Hainzl <johannes@hainzl.org>
2013-07-10 21:52:56 +0000

    Adding reading fs

--- web.js
+++ web.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
+var buffer = new Buffer(fs.readFileSync('index.html'), "utf-8")
+
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
